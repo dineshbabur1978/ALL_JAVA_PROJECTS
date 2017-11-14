ac1645748c024d8efc08ecccacc6a7251baf93ec
dinesh babu r <dineshbabur1978@gmail.com>
2017-11-14 11:40:13 +0530

    Initial Commit on Dashboard_2020 Project

---
+++ Dashboard_2020/src/com/util/ScreenRecorder.java
@@ -0,0 +1,17 @@
+package com.util;
+
+public class ScreenRecorder {
+
+	public ScreenRecorder()
+	{
+		
+		
+	}
+	
+	public int doRecording()
+	{
+		//to-do Implement Screen Recording
+		return -1;
+	}
+	
+}
